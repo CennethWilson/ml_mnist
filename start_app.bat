9c1392718d3a711df6e3bce6396079e2a30e0a77
CennethWilson <116559838+CennethWilson@users.noreply.github.com>
2025-07-08 14:35:26 +0700

    Fixed start_app.bat

--- start_app.bat
+++ start_app.bat
@@ -1,18 +1,8 @@
 @echo off
 cd /d %~dp0
 
-netstat -ano | find ":5000" > nul
-if errorlevel 1 (
-    start "" /B backend\backend.exe
-)
+start /B backend\backend.exe
 
-:waitloop
-powershell -Command "try { $r = Invoke-WebRequest -Uri http://127.0.0.1:5000/ping -UseBasicParsing; exit 0 } catch { exit 1 }"
-if errorlevel 1 (
-    timeout /t 1 > nul
-    goto waitloop
-)
+timeout /t 2 /nobreak > NUL
 
-start "" /WAIT ML_mnist.exe
-
-taskkill /F /IM backend.exe > nul 2>&1
+start "" ML_mnist.exe
